02ab3d7dd080346c448029b51774d615f17173f1
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2021-06-23 20:22:21 +0200

    Remove GSOC specific stuff

--- resources/js/directives/students.js
+++
@@ -1,9 +0,0 @@
-angular.module('coala')
-    .directive('students', ['$http', function ($http) {
-        return {
-            restrict: 'E',
-            templateUrl: '/partials/tabs/students.html',
-            controller: function ($scope, $rootScope) { },
-            controllerAs: "gsoc"
-        }
-    }]);
